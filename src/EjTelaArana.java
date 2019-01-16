0c44c97ef0c9d4632d448c37557aca40597048d6
20624677 <20624677@N205-06.alumnos.uem.es>
2019-01-16 09:24:35 +0100

    Primer Commit

---
+++ src/EjTelaArana.java
@@ -0,0 +1,63 @@
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/** Cuántos elefantes pueden llegar a balancearse encima de una tela de araña antes de que se rompa? La canción no arroja ninguna luz sobre este
+ * particular, pero quizá tú puedas ayudarnos. Entrada Cada caso, leído de la entrada estándar, consiste en dos líneas. La primera contiene el peso máximo
+ * soportado por la tela de araña. La segunda contiene los pesos de cada elefante, separados por espacio, en el orden en el que van llegando, y
+ * termina con un 0. Todos los números son enteros positivos menores que 109. La entrada acaba también con un 0, que no deberá procesarse. Salida Para cada
+ * caso de prueba, el programa indicará, en la salida estándar, el número de elefantes que pudieron balancearse en la tela, antes de que ésta se rompiera
+ * o de que no hubiera más elefantes dispuestos a arriesgarse. Los elefantes se van incorporando a la diversión por estricto orden de llegada. */
+
+public class EjTelaArana {
+
+	public static void main(String[] args) {
+
+		Scanner sca = new Scanner(System.in);
+		int suma;
+		int token;
+		int cont;
+		boolean boo = true;
+		int pesTotal;
+		int[] pesoEle;
+		StringTokenizer pesos;
+
+		System.out.println("Número de entradas: ");
+		int numEntradas = sca.nextInt();
+		sca.nextLine();
+		pesoEle = new int[numEntradas];
+
+		for (int i = 0; i < numEntradas; i++) {
+			System.out.println("Entrada " + (i + 1) + " (peso total soportado): ");
+			pesTotal = sca.nextInt();
+			sca.nextLine();
+			System.out.println("Introduce el peso de los elefantes separados por espacio y terminado en 0: ");
+			pesos = new StringTokenizer(sca.nextLine());
+
+			boo = true;
+			suma = 0;
+			cont = 0;
+
+			while (pesos.hasMoreTokens() && boo) {
+				token = Integer.parseInt(pesos.nextToken());
+				if (token != 0 && token < 109) {
+					suma = suma + token;
+					if (suma <= pesTotal) {
+						cont++;
+						pesoEle[i] = cont;
+					} else {
+						boo = false;
+					}
+				}
+			}
+		}
+		sca.close();
+		
+		System.out.println("Salida (nº de elefantes balanceados): ");
+		for (int n = 0; n < pesoEle.length; n++) {
+			System.out.println(pesoEle[n]);
+		}
+
+	}
+
+}
